630684d12248f00c4114a66e946358851b8d3e17
Stenio Almeida <stenioas@gmail.com>
2021-08-29 20:23:30 -0300

    feat: implementação para exibir os leads na página

--- react/LeadsList.tsx
+++ react/LeadsList.tsx
@@ -1,96 +1,61 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-// import { useCssHandles } from 'vtex.css-handles'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-
-// const CSS_HANDLES = ['container', 'title', 'form', 'input', 'button', 'registered']
-interface Client{
-  id: string,
-  category: string,
-  name: string,
-  email: string,
-  phoneNumber: string,
-  dateBecameLead: string,
+type Lead = {
+  id: string
+  category: string
+  name: string
+  email: string
+  phoneNumber: string
+  dateBecameLead: string
   dateBecomeClient: string
 }
 
+export default function LeadsList() {
+  const [listLeads, setListLeads] = useState([])
 
+  const api = axios.create({
+    baseURL: 'https://stenioas--hiringcoders202115.myvtex.com',
+  })
 
-const api = axios.create({
-  baseURL: "https://juliabrz--hiringcoders202115.myvtex.com"
-})
-
-const LeadsList =  () => {
-
-  const [listLeads, setListLeads] = useState(Array());
-  let lista;
-
-  //POR ALGUM MOTIVO NÃO ESTÁ SETANDO O LIST DEPOIS DE FAZER A REQUISIÇÃO
-  //o array LISTA está recebendo os dados corretos (conforme console), porém não entra no setList
-  useEffect( () => {
-    let mounted = true;
-    api.get("/leads")
-    .then(res => {
-      lista = res.data;
-      console.log(lista)
-      if(mounted){
-        console.log(lista.Items);
-        setListLeads(lista.Items);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  }, []);
-  console.log(listLeads)
-  // const handles = useCssHandles(CSS_HANDLES)
-
-  const renderItems = () => {
-    return listLeads.map((item:Client, index:number) => {
-      return (
-          <tr key={index}>
-            <td>{item.id}</td>
-            <td>{item.category}</td>
-            <td>{item.name}</td>
-            <td>{item.email}</td>
-            <td>{item.phoneNumber}</td>
-          </tr>
-      )
-    })
-  }
-
-  const renderVazio = () => {
-    return (
-        <tr>
-          <td> </td>
-          <td> </td>
-          <td> </td>
-          <td> </td>
-          <td> </td>
-        </tr>
-        )
-      }
+  useEffect(() => {
+    api
+      .get('leads')
+      .then((response) => {
+        setListLeads(response.data.Items)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [])
 
   return (
     <>
       <h1>Leads List</h1>
-      {/* Depois de completar o setList, necessário map para exibir leads */}
       <table width="90%">
         <thead>
           <tr>
-            <th>ID</th>
-            <th>TYPE</th>
-            <th>NAME</th>
+            <th>NOME</th>
             <th>E-MAIL</th>
-            <th>PHONE NUMBER</th>
+            <th>TIPO</th>
+            <th>TELEFONE</th>
           </tr>
         </thead>
         <tbody>
-          {listLeads !== [] ? renderItems : renderVazio}
+          {listLeads
+            ? listLeads.map((item: Lead, index: number) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.category}</td>
+                    <td>{item.phoneNumber}</td>
+                  </tr>
+                )
+              })
+            : 'Nenhum registro encontrado!'}
         </tbody>
       </table>
     </>
   )
 }
-
-export default LeadsList
